edce538054659bf8a99972452766570686f53d8e
Lorenzo-St <chd_frost@yahoo.com>
2023-07-18 16:20:44 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Render API.cpp
+++ Mini_Maniacs/Render API.cpp
@@ -60,7 +60,7 @@ void RenderFront::Update(void)
   Time last = timeMarker;
   timeMarker = std::chrono::high_resolution_clock::now();
   std::chrono::high_resolution_clock::duration delta = timeMarker - last;
-  long long sleep_duration = frameRateMillis - delta.count();
+  long long sleep_duration = frameRateMillis - std::chrono::duration_cast<std::chrono::microseconds>(delta).count();
 #ifdef _DEBUG
   std::cout << "Start Time: " 
             << last.time_since_epoch().count() 
