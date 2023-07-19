b77909690f748fcaea5a228381788ccce9700d21
Lorenzo-St <chd_frost@yahoo.com>
2023-07-18 21:21:40 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Main.cpp
+++ Mini_Maniacs/Main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
   SDL_Event event;
   bool running = true;
   front.SetTranslation({ 0, -100 });
-  front.SetScale({100, 200 });
+  front.SetScale({300, 200 });
   while (running)
   {
     SDL_PollEvent(&event);
