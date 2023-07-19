ccce29c1bec991f72a794807b5d01974c55a8594
Lorenzo-St <chd_frost@yahoo.com>
2023-07-19 10:06:36 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Render API.h
+++ Mini_Maniacs/Render API.h
@@ -36,7 +36,7 @@ private:
   
   int Width = 1280;
   int Height = 720;
-  int TargetFrameRate = 244;
+  int TargetFrameRate = 60;
   long long frameRateMillis = 0;
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
