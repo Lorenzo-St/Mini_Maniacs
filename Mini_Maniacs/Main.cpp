37f87a95d70315b5820897eef96b779d7c581ca4
Lorenzo-St <chd_frost@yahoo.com>
2023-07-20 17:37:19 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Main.cpp
+++ Mini_Maniacs/Main.cpp
@@ -41,8 +41,8 @@ int main(int argc, char* argv[])
     api.SetTexture(t);
     api.Draw(verticies);
     api.SetTexture(nullptr);
-    //pos.x += xSpeed * Time.deltaTime();
-    //rot += 1 * Time.deltaTime();
+    pos.x += xSpeed * Time.deltaTime();
+    rot += 1 * Time.deltaTime();
 
     if (pos.x >= api.GetWindowWidth() / 2.0f + scale.x)
       pos.x = -(api.GetWindowWidth() / 2.0f + scale.x);
