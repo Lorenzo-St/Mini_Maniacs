9fc9933ef43cc170209a48036af661fd652ad4f4
Lorenzo-St <chd_frost@yahoo.com>
2023-07-27 10:37:44 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Temp.h
+++ Mini_Maniacs/Temp.h
@@ -9,20 +9,12 @@
 class TempScene : public Scene
 {
 public:
-  mesh* m;
-  
   Entity* e;
   void Init() 
   {
     e = EntitySystem::GetActive().CreateEntity("Entity");
 
-    m = api.CreateMesh();
-    m->AddVertex({ -.5f, -.5f }, { 255,   0, 255, 255 }, { 0, 1 });
-    m->AddVertex({ -.5f,  .5f }, {   0, 255, 255, 255 }, { 0, 0 });
-    m->AddVertex({  .5f,  .5f }, { 255, 255, 255, 255 }, { 1, 0 });
-    m->AddVertex({  .5f,  .5f }, { 255, 255, 255, 255 }, { 1, 0 });
-    m->AddVertex({  .5f, -.5f }, { 255, 255,   0, 255 }, { 1, 1 });
-    m->AddVertex({ -.5f, -.5f }, { 255,   0, 255, 255 }, { 0, 1 });
+
     updates = 0;
     Random<float> r;
     api.SetMatrixData({ r.Next(-200.f, 200.f), r.Next(-200.f, 200.f) }, {100, 100}, 0);
@@ -32,13 +24,9 @@ public:
     ++updates;
     if (updates >= 400)
       getSceneSystem()->SetNextScene(this);
-    api.SetMatrixData({ 0,0 }, { 100,100 }, 0);
-    m->Draw();
   }
   void Exit() 
   {
-    m->Destroy();
-    m = nullptr;
     ++restarts;
     EntitySystem::GetActive().DestroyAll();
   }
