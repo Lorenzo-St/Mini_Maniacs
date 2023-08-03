9258d1acb2e2326043de6fa60a07c96a141cc4d0
Lorenzo-St <chd_frost@yahoo.com>
2023-08-03 16:01:55 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Temp.h
+++ Mini_Maniacs/Temp.h
@@ -21,7 +21,11 @@ public:
     floor->AddComponent(new Transform());
     floor->GetComponent<Transform>()->SetPosition({ 0, -100 });
     floor->GetComponent<Transform>()->SetScale({ 1000, 1 });
-    floor->GetComponent<RectCollider>()->addLine({ -500, 0 }, { 500, 0 });
+    floor->GetComponent<RectCollider>()->addLine({  500,  0 },  { -500,  0 });
+    floor->GetComponent<RectCollider>()->addLine({ -500, -5 },  {  500, -5 });
+    floor->GetComponent<RectCollider>()->addLine({ -500,  0 },  { -500, -5 });
+    floor->GetComponent<RectCollider>()->addLine({  500, -5 },  { -500,  0 });
+
     EntitySystem::GetActive().AddEntity(floor);
     updates = 0;
     Random<float> r;
