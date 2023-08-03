83963b8af34eb15a34f2ca2888a4ecec9bf7a604
Lorenzo-St <chd_frost@yahoo.com>
2023-08-03 15:27:55 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Main.cpp
+++ Mini_Maniacs/Main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
   Engine engine;
   engine.addSystem(new SceneSystem());
   engine.addSystem(new EntitySystem());
-  api.setZoom(5);
+  api.setZoom(2);
   engine.Init();
   Input.addBinding(Enter, { SDLK_g });
   
