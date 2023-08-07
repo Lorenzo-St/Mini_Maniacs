ba57aea1196eb79082711bd37fde65ebb73b5933
Lorenzo-St <chd_frost@yahoo.com>
2023-08-07 10:31:36 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Render API.cpp
+++ Mini_Maniacs/Render API.cpp
@@ -155,14 +155,14 @@ void RenderFront::DrawEllipse(glm::vec2 pos, float rad) const
   const glm::mat4x4 proj = glm::ortho<float>(-Width * 1.0f, Width * 1.0f, -Height * 1.0f, Height * 1.0f);
   std::array<SDL_Vertex, 30> ellip = {};
   SDL_FPoint cen = convert(pos);
-  for (float angle = 0, i = 0; angle < 360; angle += 30, i+=2) 
+  for (float angle = 0, i = 0; angle < 360; angle += 15, i+=2) 
   {
     ellip[i  ].position.x = (pos.x + rad * std::sinf((M_PI * angle) / 180));
     ellip[i  ].position.y = (pos.y + rad * std::cosf((M_PI * angle) / 180));
     ellip[i+1].position.x = (pos.x);
     ellip[i+1].position.y = (pos.y);
-    ellip[i+2].position.x = (pos.x + rad * std::sinf((M_PI * (angle + 30)) / 180));
-    ellip[i+2].position.y = (pos.y + rad * std::cosf((M_PI * (angle + 30)) / 180));
+    ellip[i+2].position.x = (pos.x + rad * std::sinf((M_PI * (angle + 15)) / 180));
+    ellip[i+2].position.y = (pos.y + rad * std::cosf((M_PI * (angle + 15)) / 180));
     ellip[i  ].position = convert(glm::vec2(glm::vec4(convert(ellip[i  ].position), 0,1) * proj * zoom));
     ellip[i  ].position = convert(convert(ellip[i  ].position) * glm::vec2(Width, -Height) + glm::vec2(Width / 2.0f + c.pos.x, Height / 2.0f + c.pos.y));
     ellip[i+1].position = convert(glm::vec2(glm::vec4(convert(ellip[i+1].position), 0, 1) * proj * zoom));
