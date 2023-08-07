21a6a43778a10223099316da715cb60b380aec1f
Lorenzo-St <chd_frost@yahoo.com>
2023-08-07 14:13:34 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -120,7 +120,7 @@ void CircleCollision(Collider* Ellip1, Collider* Ellip2)
     return;
   glm::vec2 projection = (glm::dot(toEllip2, moveVec) / glm::dot(moveVec, moveVec)) * moveVec;
   if (glm::distance(startPos, projection) > glm::length(moveVec))
-    return;
+    projection = endPos;
   glm::vec2 closestPoint = startPos + projection;
 
   float distance = glm::distance(closestPoint, ellip2Pos);
