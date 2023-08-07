2215221c4446d8dbaa47a9d0062da1f52a170cfd
Lorenzo-St <chd_frost@yahoo.com>
2023-08-07 10:38:05 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/Render API.cpp
+++ Mini_Maniacs/Render API.cpp
@@ -164,8 +164,8 @@ void RenderFront::DrawEllipse(glm::vec2 pos, float rad) const
     ellip[i  ].position.y = (pos.y + rad * std::cosf((M_PI * angle) / 180));
     ellip[i+1].position.x = (pos.x);
     ellip[i+1].position.y = (pos.y);
-    ellip[i+2].position.x = (pos.x + rad * std::sinf((M_PI * (angle + 15)) / 180));
-    ellip[i+2].position.y = (pos.y + rad * std::cosf((M_PI * (angle + 15)) / 180));
+    ellip[i+2].position.x = (pos.x + rad * std::sinf((M_PI * (angle + 30)) / 180));
+    ellip[i+2].position.y = (pos.y + rad * std::cosf((M_PI * (angle + 30)) / 180));
     ellip[i  ].position = convert(glm::vec2(glm::vec4(convert(ellip[i  ].position), 0,1) * proj * zoom));
     ellip[i  ].position = convert(convert(ellip[i  ].position) * glm::vec2(Width, -Height) + glm::vec2(Width / 2.0f + c.pos.x, Height / 2.0f + c.pos.y));
     ellip[i+1].position = convert(glm::vec2(glm::vec4(convert(ellip[i+1].position), 0, 1) * proj * zoom));
@@ -173,6 +173,7 @@ void RenderFront::DrawEllipse(glm::vec2 pos, float rad) const
     ellip[i+2].position = convert(glm::vec2(glm::vec4(convert(ellip[i+2].position), 0, 1) * proj * zoom));
     ellip[i+2].position = convert(convert(ellip[i+2].position) * glm::vec2(Width, -Height) + glm::vec2(Width / 2.0f + c.pos.x, Height / 2.0f + c.pos.y));
   }
+
   SDL_RenderGeometry(renderer, activeTexture, ellip.data(), static_cast<int>(ellip.size()), nullptr, 0);
 
 }
