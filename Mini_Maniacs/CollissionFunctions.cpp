d4c86869f55e998dea90516254b2df4e0969f21f
Lorenzo-St <chd_frost@yahoo.com>
2023-08-07 13:00:31 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -115,7 +115,7 @@ void CircleCollision(Collider* Ellip1, Collider* Ellip2)
 
   glm::vec2 toEllip2 = ellip2Pos - startPos;
 
-  glm::vec2 projection = (glm::dot(toEllip2, moveVec) / glm::length(moveVec)) * moveVec;
+  glm::vec2 projection = (glm::dot(toEllip2, moveVec) / glm::dot(moveVec, moveVec)) * moveVec;
 
   if (glm::length(projection) > glm::length(moveVec))
     return;
