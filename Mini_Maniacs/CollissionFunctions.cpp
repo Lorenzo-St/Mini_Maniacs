37c502541dd974e3d0204d7fa02f509f00448b0e
Lorenzo-St <chd_frost@yahoo.com>
2023-08-08 12:33:23 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -142,7 +142,7 @@ void CircleCollision(Collider* Ellip1, Collider* Ellip2)
     workingRot = glm::normalize(workingRot);
     glm::vec2 offSetVector = workingRot * len;
     Ellip1->GetParent()->GetComponent<Transform>()->SetPosition(closestPoint + offSetVector);
-    //len = glm::length(Ellip1->GetParent()->GetComponent<Transform>()->GetVelocity());
+    len = glm::length(Ellip1->GetParent()->GetComponent<Transform>()->GetVelocity());
     Ellip1->GetParent()->GetComponent<Transform>()->AddVelocity(glm::normalize(staticToClosest) * len);
 
 
