a0e725c2d476469e6763669ddd7cd0058f3a7115
Lorenzo-St <chd_frost@yahoo.com>
2023-08-14 16:28:37 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -74,23 +74,27 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
           wallVec = segment[0] - segment[1];
           if (glm::dot(wallVec, testVector) > 0)
             dir = 1;
-          testVector = (testPos + xMove) - segment[1];
-          wallVec = segment[1] - segment[0];
-          if (glm::dot(wallVec, testVector) < 0)
-            dir = 1;
-
+          else 
+          {
+            testVector = (testPos + xMove) - segment[1];
+            wallVec = segment[1] - segment[0];
+            if (glm::dot(wallVec, testVector) < 0)
+              dir = 1;
+          }
         }
         else if (glm::dot(wallNorm, testPos) >= glm::dot(wallNorm, segment[0]) && glm::dot(wallNorm, testPos + yMove) >= glm::dot(wallNorm, segment[1]))
         {
           testVector = (testPos + xMove) - segment[0];
           wallVec = segment[0] - segment[1];
-          if (glm::dot(wallVec, testVector) < 0)
-            continue;
-          testVector = (testPos + xMove) - segment[1];
-          wallVec = segment[1] - segment[0];
-          if (glm::dot(wallVec, testVector) < 0)
-            continue;
-          dir = 0;
+          if (glm::dot(wallVec, testVector) > 0)
+            dir = 0;
+          else
+          {
+            testVector = (testPos + xMove) - segment[1];
+            wallVec = segment[1] - segment[0];
+            if (glm::dot(wallVec, testVector) < 0)
+              dir = 0;
+          }
         }
         else
           dir = 2;
