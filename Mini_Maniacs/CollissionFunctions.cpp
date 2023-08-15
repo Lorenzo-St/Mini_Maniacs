48db6a8fb1f88a0f5c03849bf8bd45c856181f2f
Lorenzo-St <chd_frost@yahoo.com>
2023-08-14 18:55:42 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -72,9 +72,9 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
         if (toOther.x == toOther.y)
           dir = 2;
         else if (toOther.y > toOther.x)
-          dir = 0;
-        else if (toOther.x > toOther.y)
           dir = 1;
+        else if (toOther.x > toOther.y)
+          dir = 0;
         earliestTime = ti;
         earliestMove = moveVec;
       }
