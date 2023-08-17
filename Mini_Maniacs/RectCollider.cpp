92f59c7f26faf96b330038b7cb31eb1c0f108707
Lorenzo-St <chd_frost@yahoo.com>
2023-08-17 11:19:32 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/RectCollider.cpp
+++ Mini_Maniacs/RectCollider.cpp
@@ -65,7 +65,7 @@ void RectCollider::OnCollision(Entity* other)
     glm::vec2 otherPos = other->GetComponent<Transform>()->GetPosition();
     glm::vec2 thisPos = this->GetParent()->GetComponent<Transform>()->GetPosition();
     glm::vec2 preserved = thisPos;
-    thisPos = { static_cast<float>(static_cast<int>(thisPos.x/tileSize) * tileSize), static_cast<float>(static_cast<int>(thisPos.y/tileSize) * tileSize)  };
+    thisPos = { static_cast<float>(static_cast<int>(glm::round(thisPos.x)/tileSize) * tileSize), static_cast<float>(static_cast<int>(glm::round(thisPos.y)/tileSize) * tileSize)  };
     RectCollider* otherR = other->GetComponent<RectCollider>();
     glm::vec2 dir = thisPos - otherPos;
     glm::vec2 velo = this->GetParent()->GetComponent<Physics>()->GetVelocity();
