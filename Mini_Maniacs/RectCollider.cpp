111b2581b729d5cb748c2d171e861c30c059c66d
Lorenzo-St <chd_frost@yahoo.com>
2023-08-17 09:17:17 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/RectCollider.cpp
+++ Mini_Maniacs/RectCollider.cpp
@@ -73,11 +73,15 @@ void RectCollider::OnCollision(Entity* other)
     dir.x = (std::abs(velo.y) > std::abs(velo.x)) ? 0: dir.x / std::abs(dir.x != 0 ? dir.x : 1);
     dir.y = (std::abs(velo.x) > std::abs(velo.y)) ? 0: dir.y / std::abs(dir.y != 0 ? dir.y : 1);
 
+    if (dir.x == 0)
+      velo.y = 0;
+    else
+      velo.x = 0;
     if (dir == glm::vec2(0, 0))
     {
       dir = thisPos - otherPos;
-      dir.x = (std::abs(dir.y) > std::abs(dir.x)) ? 0 : dir.x / std::abs(dir.x != 0 ? dir.x : 1), velo.x = 0;
-      dir.y = (std::abs(dir.x) > std::abs(dir.y)) ? 0 : dir.y / std::abs(dir.y != 0 ? dir.y : 1), velo.y = 0;
+      dir.x = (std::abs(dir.y) > std::abs(dir.x)) ? 0 : dir.x / std::abs(dir.x != 0 ? dir.x : 1);
+      dir.y = (std::abs(dir.x) > std::abs(dir.y)) ? 0 : dir.y / std::abs(dir.y != 0 ? dir.y : 1);
     }
     dir *= otherR->tileSize;
     glm::vec2 nearestTile = otherPos + dir;
