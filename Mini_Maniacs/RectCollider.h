8a99ac5ef19b5d5b01e54555ebc52dff13961bd3
Lorenzo-St <chd_frost@yahoo.com>
2023-08-17 11:24:43 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/RectCollider.h
+++ Mini_Maniacs/RectCollider.h
@@ -9,10 +9,9 @@ class RectCollider : public Collider
 {
 public:
   RectCollider() { }
-  RectCollider(RectCollider const* r): w(r->w), h(r->h) { linesSegments = r->linesSegments; };
+  RectCollider(RectCollider const* r) : w(r->w), h(r->h) { return; };
   RectCollider* Clone() { return new RectCollider(this); };
   void Read(Stream* s);
-  void addLine(glm::vec2 start, glm::vec2 end) { linesSegments.push_back({ start, end }); }
 
   float Width() { return w; }
   float Height() { return h; }
@@ -20,7 +19,6 @@ public:
 
   void OnCollision(Entity* other);
 
-  std::vector<std::array<glm::vec2, 2>> const& getSegments(void) { return linesSegments; }
 private:
   // -------------------------
   // Required Component things
@@ -29,7 +27,6 @@ private:
   const bool s = set();
   // -------------------------
   float w, h;
-  std::vector<std::array<glm::vec2, 2>> linesSegments;
   
 };
 
