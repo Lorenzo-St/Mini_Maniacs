23c19f2ea6f5cffe9d928e7e5b0809cd40892e14
Lorenzo-St <chd_frost@yahoo.com>
2023-08-22 16:08:52 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -42,8 +42,8 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
   }
   glm::vec2 preserved = NewPosition;
   glm::vec2 moveVec = rect1->GetParent()->GetComponent<Physics>()->GetVelocity();
-  if (std::abs(moveVec.x) > 100)
-    moveVec.x = 10 * moveVec.x / std::abs(moveVec.x);
+  if (std::abs(moveVec.y) > 100)
+    moveVec.y = 10 * moveVec.y / std::abs(moveVec.y);
   std::cout << moveVec << std::endl;
   glm::vec2 testPoint = OldPosition;
 
