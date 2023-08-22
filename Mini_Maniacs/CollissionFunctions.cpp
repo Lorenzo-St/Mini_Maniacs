87bba79c646144fe2c258fb3637319f2250c7db9
Lorenzo-St <chd_frost@yahoo.com>
2023-08-22 16:31:15 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -40,21 +40,21 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
     //std::cout << "Trigger Collision" << std::endl;
     return;
   }
-  glm::vec2 preserved = NewPosition;
+  glm::vec2 preserved = OldPosition;
   glm::vec2 moveVec = rect1->GetParent()->GetComponent<Physics>()->GetVelocity();
   if (std::abs(moveVec.y) > 20)
     moveVec.y = 20 * moveVec.y / std::abs(moveVec.y);
   glm::vec2 testPoint = OldPosition;
 
   //testPoint.x += moveVec.x;
-  //if (std::abs(testPoint.x - WallPos.x) > MOffset.x + WOffset.x == false)
-  //{
-  //  preserved.x = OldPosition.x;
-  //}
-  testPoint.y += moveVec.y;
-  if (std::abs(testPoint.y - WallPos.y) > MOffset.y + WOffset.y == false) 
+  if (std::abs(testPoint.x - WallPos.x) > MOffset.x + WOffset.x)
   {
-    preserved.y = OldPosition.y;
+    preserved.x = NewPosition.x;
+  }
+  testPoint.y += moveVec.y;
+  if (std::abs(testPoint.y - WallPos.y) > MOffset.y + WOffset.y) 
+  {
+    preserved.y = NewPosition.y;
 
   }
 
