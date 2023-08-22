17f116993b30e42b5bab02fdc2308bbedcc01f7f
Lorenzo-St <chd_frost@yahoo.com>
2023-08-22 16:32:14 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -44,8 +44,8 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
   glm::vec2 moveVec = rect1->GetParent()->GetComponent<Physics>()->GetVelocity();
   if (std::abs(moveVec.y) > 20)
     moveVec.y = 20 * moveVec.y / std::abs(moveVec.y);
-  if (std::abs(moveVec.x) > 20)
-    moveVec.x = 20 * moveVec.x / std::abs(moveVec.x);
+  if (std::abs(moveVec.x) > 25)
+    moveVec.x = 25 * moveVec.x / std::abs(moveVec.x);
   glm::vec2 testPoint = OldPosition;
 
   testPoint.x += moveVec.x;
