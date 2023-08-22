3574db47fd24477f3059829296aa0b4f3de1993e
Lorenzo-St <chd_frost@yahoo.com>
2023-08-22 15:56:15 -0700

    Succesful Debug build on x64

--- Mini_Maniacs/CollissionFunctions.cpp
+++ Mini_Maniacs/CollissionFunctions.cpp
@@ -40,18 +40,18 @@ void RectangleCollision(Collider* rect1, Collider* rect2)
     //std::cout << "Trigger Collision" << std::endl;
     return;
   }
-  glm::vec2 preserved = NewPosition;
+  glm::vec2 preserved = OldPosition;
   glm::vec2 moveVec = NewPosition - OldPosition;
   glm::vec2 testPoint = OldPosition;
   testPoint.x += moveVec.x;
-  if (!std::abs(testPoint.x - WallPos.x) > MOffset.x + WOffset.x)
+  if (std::abs(testPoint.x - WallPos.x) > MOffset.x + WOffset.x)
   {
-    preserved.x = OldPosition.x;
+    preserved.x = NewPosition.x;
   }
   testPoint.y += moveVec.x;
-  if (!std::abs(testPoint.y - WallPos.y) >= MOffset.y + WOffset.y) 
+  if (std::abs(testPoint.y - WallPos.y) > MOffset.y + WOffset.y) 
   {
-    preserved.y = OldPosition.y;
+    preserved.y = NewPosition.y;
 
   }
 
