f8d888ec2f38c034cdf2500295c9ce1d59c242e8
Lorenzo-St <chd_frost@yahoo.com>
2023-08-23 19:36:44 -0700

    Succesful Editor build on x64

--- Mini_Maniacs/Scene.cpp
+++ Mini_Maniacs/Scene.cpp
@@ -10,15 +10,15 @@
 #ifdef EDITOR
 void Scene::Write(std::ofstream* s) 
 {
-  *s << "<Scene>\n" << "<Name>\n" << name() << "\n<Entities>\n";
+  *s << "<Scene>\n" << "<Name>\n" << name() << "\n<Entities>\n ";
   
   for (auto& e : entities) 
   {
     if (e->isPrefabRoot() || e->isPrefabChild())
       continue;
-    *s << e->getProto() << "\n";
-    *s << "<Position>\n";
-    *s << e->GetComponent<Transform>()->StartingPosition() << "\n";
+    *s << e->getProto() << "\n ";
+    *s << "<Position>\n  ";
+    *s << e->GetComponent<Transform>()->StartingPosition() << "\n ";
   }
   *s << "</Entities>\n <Prefabs>\n";
   for (auto& p : prefabs) 
@@ -32,56 +32,56 @@ void Scene::Write(std::ofstream* s)
 #endif
 
 void Scene::ReadFile(const char* c)
+{
+  Stream s(c);
+  std::string token;
+  ReadSection rs = Default;
+  Entity* active = nullptr;
+  while (true)
   {
-    Stream s(c);
-    std::string token;
-    ReadSection rs = Default;
-    Entity* active = nullptr;
-    while (true)
+    token = s.ReadString();
+    if (rs == Default)
     {
-      token = s.ReadString();
-      if (rs == Default)
+      if (token == "<Name>")
+        name(s.ReadString());
+      else if (token == "<Entities>")
+        rs = Entities;
+      else if (token == "<Prefabs>")
+        rs = Prefabs;
+      else if (token == "")
+        break;
+    }
+    else if (rs == Entities)
+    {
+      if (token == "</Entities>")
       {
-        if (token == "<Name>")
-          name(s.ReadString());
-        else if (token == "<Entities>")
-          rs = Entities;
-        else if (token == "<Prefabs>")
-          rs = Prefabs;
-        else if (token == "")
-          break;
+        rs = Default;
       }
-      else if (rs == Entities)
+      else if (token == "<Position>")
+        active->GetComponent<Transform>()->SetPosition(s.ReadVector());
+      else
       {
-        if (token == "</Entities>")
-        {
-          rs = Default;
-        }
-        else if (token == "<Position>")
-          active->GetComponent<Transform>()->SetPosition(s.ReadVector());
-        else 
-        {
-          active = EntitySystem::GetActive().CreateEntity(token.c_str());
+        active = EntitySystem::GetActive().CreateEntity(token.c_str());
 #ifdef EDITOR
-          entities.push_back(active);
+        entities.push_back(active);
 #endif
-        }
       }
-      else if (rs == Prefabs) 
+    }
+    else if (rs == Prefabs)
+    {
+      if (token == "</Prefabs>")
       {
-        if (token == "</Prefabs>")
-        {
-          rs = Default;
-        }
-        else 
-        {
-          active = EntitySystem::GetActive().CreatePrefab(token.c_str());
+        rs = Default;
+      }
+      else
+      {
+        active = EntitySystem::GetActive().CreatePrefab(token.c_str());
 #ifdef EDITOR
-          prefabs.push_back(active);
+        prefabs.push_back(active);
 
 #endif
-        
-        }
-        }
+
+      }
     }
   }
+}
