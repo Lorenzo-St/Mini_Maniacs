6be398b8620a646370975641db31eec460b5b9e9
Lorenzo-St <chd_frost@yahoo.com>
2023-08-26 11:36:04 -0700

    Succesful Editor build on x64

--- Mini_Maniacs/EditorSystem.cpp
+++ Mini_Maniacs/EditorSystem.cpp
@@ -138,7 +138,7 @@ void EditorSystem::DrawParentMenu(void)
   if (row < 1)
     row = 1;
 
-  glm::vec2 startingPos = { BGPos.x - (BGScale.x * .42f), BGPos.y + (BGScale.y * .25f) };
+  glm::vec2 startingPos = { BGPos.x - (BGScale.x * .41f), BGPos.y + (BGScale.y * .25f) };
   glm::vec2 BoxScale = { BGScale.x / static_cast<float>(xCount + 2), 0 };
   BoxScale.y = BoxScale.x;
   
