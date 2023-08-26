14c507564897e9dfb4b71a21bd4ed763416f35c5
Lorenzo-St <chd_frost@yahoo.com>
2023-08-26 11:26:05 -0700

    Succesful Editor build on x64

--- Mini_Maniacs/EditorSystem.cpp
+++ Mini_Maniacs/EditorSystem.cpp
@@ -151,7 +151,7 @@ void EditorSystem::DrawParentMenu(void)
     if (i / xCount >= yCount + (row - 1))
       break;
 
-    glm::vec2 pos = startingPos + glm::vec2(BoxScale.x * ((i / row) % (xCount - 1)) * 1.1f, -BoxScale.y * ((i / row) / xCount) * 1.1f);
+    glm::vec2 pos = startingPos + glm::vec2(BoxScale.x * (i % (xCount - 1)) * 1.1f, -BoxScale.y * (i / xCount) * 1.1f);
     if (PointInRect(mousePos, pos, BoxScale))
     {
 
