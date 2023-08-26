765c473fd40faddc2537f471d04864c9f858724b
Lorenzo-St <chd_frost@yahoo.com>
2023-08-26 09:30:26 -0700

    Succesful Editor build on x64

--- Mini_Maniacs/EditorSystem.cpp
+++ Mini_Maniacs/EditorSystem.cpp
@@ -134,7 +134,7 @@ void EditorSystem::DrawObjectMenu(void)
   int i = 0;
   for (auto const& e : col) 
   {
-    api.DrawRect(startingPos + glm::vec2(BoxScale.x * (i % xCount), BoxScale.y * (i / yCount)), BoxScale);
+    api.DrawRect(startingPos + glm::vec2(BoxScale.x * (i % xCount), BoxScale.y * (i / xCount)), BoxScale);
     ++i;
   }
   
